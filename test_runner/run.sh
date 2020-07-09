c17811c98ed75f977c468ea4e15ce0eae0193530
roozbeh sharifnasab <rsharifnasab@gmail.com>
2020-07-09 15:20:43 +0430

    added auto zip ability

--- test_runner/run.sh
+++ test_runner/run.sh
@@ -19,7 +19,7 @@ echo "comiling project"
 javac -cp .:junit.jar *.java || exit
 echo "compile complete"
 
-java -cp .:junit.jar:j2.jar  org.junit.runner.JUnitCore MyTest
+java -cp .:junit.jar:j2.jar  org.junit.runner.JUnitCore MyTest || exit
 echo "run complete"
 
 cd ..
@@ -27,3 +27,10 @@ cd ..
 echo "deleting sandbox"
 find . -name '*.class' -delete
 rm -r ./sandbox
+
+echo "updating zip file"
+rm t.zip > /dev/null 2>&1
+
+cd tests
+zip -r ../t.zip *
+
