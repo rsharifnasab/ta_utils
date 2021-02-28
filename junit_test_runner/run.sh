5c4a44052058760dbb3ce589018db6d1f08091d4
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2021-03-01 00:16:52 +0330

    better run.sh

--- junit_test_runner/run.sh
+++ junit_test_runner/run.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
+
+set -e
+
+start_point="$(pwd)"
+
+
+try_delete(){
+    rm -r "$1" > /dev/null 2>&1 || true
+}
+
+
+clean() {
+    cd "$start_point"
+    try_delete "sandbox"
+    exit
+}
+
 clear
 
 find . -name '*.class' -delete
-rm -r sandbox > /dev/null 2>&1
+try_delete "sandbox"
 
 mkdir sandbox
 
@@ -12,31 +29,45 @@ cp -r tests/project/* sandbox/ > /dev/null 2>&1 || echo "project folder is empty
 cp -r toTest/* sandbox/
 cp -r tests/test/* sandbox/
 
-cd sandbox/ || exit
+cd sandbox/
 
 echo "comiling project"
 find . -name "*.java" > sources.txt
-javac -cp .:junit.jar @sources.txt || exit
+javac -cp .:junit.jar @sources.txt || clean
 
-echo "running SampleTest"
-java -cp .:junit.jar:j2.jar  org.junit.runner.JUnitCore SampleTest || exit
 
 echo "running MyTest"
-java -cp .:junit.jar:j2.jar  org.junit.runner.JUnitCore MyTest || exit
+for testFile in ./*Test.java; do
+    className="$(basename "$testFile" .java)"
+    echo "class name : \"$className\""
+    java -cp .:junit.jar:j2.jar org.junit.runner.JUnitCore \
+        "$className"  \
+        || clean
+done
 echo "run complete"
 
 cd ..
 
 echo "deleting sandbox"
 find . -name '*.class' -delete
-# rm -r ./sandbox
-
-echo "updating zip file"
-rm t.zip > /dev/null 2>&1
-
-cd tests || exit
-zip -r ../t.zip ./
+rm -r ./sandbox
 
 
 
 
+
+echo "updating solution file"
+try_delete "sol.zip"
+cd toTest || clean
+zip -r ../sol.zip ./
+cd ..
+
+
+echo "updating tests zip file"
+try_delete "tests.zip"
+cd tests || clean
+zip -r ../tests.zip ./
+cd ..
+
+
+clean
