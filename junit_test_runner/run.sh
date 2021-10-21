36a3696fb1dc21e9a3407555061c3aea64bcd714
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2021-10-21 11:24:06 +0330

    Remove trap for runner.sh

--- junit_test_runner/run.sh
+++ junit_test_runner/run.sh
@@ -3,11 +3,6 @@
 set -o errexit
 set -o nounset
 
-# keep track of the last executed command
-trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-# echo an error message before exiting
-trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
-
 start_point="$(pwd)"
 
 when_fail="--allow"
