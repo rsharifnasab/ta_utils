04a8d1cc25b4524a3ca21d6ac43db2c383bd4350
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2022-02-02 14:27:10 +0330

    rename pandas sum

--- other_scripts/pandas_sum/code_lms.py
+++ other_scripts/pandas_sum/code_lms.py
@@ -1,37 +1,27 @@
+import argparse
+
 import numpy as np
 import pandas as pd
 
-import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--file", "-f", help="file path")
 args = parser.parse_args()
 FILE_PATH = args.file
 
 
-def score_ok(df, row):
-    effective_row = row*5 + 1
-    col = df[f"Unnamed: {effective_row}"]
-    assert col[0] == "نمره داوری با تاخیر"
-    score = col[1:].astype(float).fillna(0)
+def score_ok(df):
+    col = df["نمره/10.00"]
+    score = col[:].astype(float).fillna(0)
 
     return score
 
 
 def main():
     df = pd.read_excel(FILE_PATH)
-    st_numbers = df['Unnamed: 1'][1:].astype(str)
-    counter = 1
-    scores = None
-    while True:
-        try:
-            score_cur = score_ok(df, counter)
-            if scores is None:
-                scores = score_cur
-            else:
-                scores += score_cur
-            counter += 1
-        except KeyError:
-            break
+    st_numbers = df["کد شناسائی"][:].astype(
+        float).fillna(0).astype(int).astype(str)
+    print(st_numbers)
+    scores = score_ok(df)
 
     scores_final = []
     scores_map = {}
@@ -46,8 +36,8 @@ def main():
         for student in f.readlines():
             student = student.strip().replace('\u200c', '')
             score = scores_map.get(student, 0)
-            #print(f"{student} {scores_map[student]}")
-            print(f"{scores_map[student]}")
+            #print(f"{student} {score}")
+            print(f"{score}")
 
 
 if __name__ == "__main__":
