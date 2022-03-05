8d74ccd94b693325ac27b7f5dac34cfc4d1b487a
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2022-03-05 22:18:17 +0330

    handle lib folder and zip inputs

--- jplag/ss-single.sh
+++ jplag/ss-single.sh
@@ -10,6 +10,9 @@ LANG="$3"       # "cpp"
 LIB_FOLDER="lib"
 RES_COUNT="100"
 
+mkdir -p "$INP_FOLDER/$LIB_FOLDER"
+
+
 case "$LANG" in
     "cpp" | "c" )
         EXTENSIONS="c,cpp"
@@ -19,6 +22,12 @@ case "$LANG" in
 esac
 
 find "$INP_FOLDER" -name "*.txt" -type f -delete
+find "$INP_FOLDER" -name "*.zip" -type f | while read -r file; do
+    echo "Unzipping $file"
+    zip_path="${file%/*}"
+    unzip -o "$file" -d "$zip_path"
+    rm "$file"
+done
 
 ASTYLE_OPTS=(--suffix=none --recursive --style=java --break-blocks --pad-comma --pad-first-paren-out --unpad-paren --add-braces --convert-tabs --delete-empty-lines)
 astyle "$INP_FOLDER/*" "${ASTYLE_OPTS[@]}"
@@ -27,7 +36,7 @@ astyle "$INP_FOLDER/*" "${ASTYLE_OPTS[@]}"
 JPLAG_JAR="./jplag.jar"
 JPLAG_CMD="java -jar $JPLAG_JAR"
 
-OPTS_LANG=(-l "$LANG" -p "$EXTENSIONS") # -bc "$LIB_FOLDER")
+OPTS_LANG=(-l "$LANG" -p "$EXTENSIONS" -bc "$LIB_FOLDER")
 OPTS_GENERAL=(-v quiet -c normal -r "$OUT_FOLDER" -n "$RES_COUNT")
 
 $JPLAG_CMD "${OPTS_GENERAL[@]}" "${OPTS_LANG[@]}" "${INP_FOLDER}"
