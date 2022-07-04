abb55652b4004097d1a8d94118226556df5a3652
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2022-07-04 19:41:12 +0430

    feat(jplag) better one question for projects

--- jplag/one_question.sh
+++ jplag/one_question.sh
@@ -4,15 +4,6 @@ set -e
 set -o pipefail
 set -u
 
-clean_zip(){ 
-    # redirect stdout and stderr to /dev/null
-    # so that we don't see the output of the unzip command
-
-    zip -d "$1" __MACOSX/\* \*/.DS_Store  > /dev/null 2>&1 || true
-}
-
-
-
 INP_FOLDER="$1"  #"./codes"
 OUT_FOLDER="$2"  #"./result"
 LANG="$3"        #"cpp"
@@ -21,6 +12,45 @@ LIB_FOLDER="$4"  #"./libs/current_question"
 LIB_NAME="lib"
 RES_COUNT="40"
 
+clean_zip(){ 
+   zip -d "$1" __MACOSX/\* \*/.DS_Store  > /dev/null 2>&1 || true
+   true
+}
+
+function flatten(){
+    return 0
+
+    for student in "$INP_FOLDER"/*; do
+        if [ -d "$student" ]; then
+            echo "$student"
+
+            find "$student" -type f | while read -r file; do
+                dest="$INP_FOLDER/$student/$(basename -- "$file")"
+                #echo "$file" "$dest"
+                [ -f "$dest" ] || mv "$file" "$dest" > /dev/null 2>&1 && {
+                    #echo "duplicate file: $file"
+                    true
+                    #return 1
+                }
+            done
+        fi
+    done
+    find "$INP_FOLDER" -type d -empty -delete    
+}
+
+function textify(){ # for example textify dart
+    find "$INP_FOLDER" -name "*.$1" -type f | while read file; do
+        mv "$file" "${file%.$1}.txt"
+    done
+}
+
+function clean_out_folder(){
+    find "$OUT_FOLDER" \( -name "match*.html" -or -name "result.html" -or -name "matches_avg.csv" \) -delete
+}
+
+function clean_in_folder(){
+    find "$INP_FOLDER" \( -name ".gitignore" -or -name ".idea" -or -name "*.class" -or -name ".git" \) -print0 | sort | uniq | xargs -0 -r -n 1 rm -r
+}
 
 case "$LANG" in
     "cpp" | "c" )
@@ -30,35 +60,45 @@ case "$LANG" in
         EXTENSIONS="$LANG"
 esac
 
-mkdir -p "$INP_FOLDER/$LIB_NAME"
-unzip -q -o "$LIB_FOLDER" -d "$INP_FOLDER/$LIB_NAME"
+ASTYLE_OPTS=(--quiet --suffix=none --recursive --style=java \
+    --break-blocks --pad-comma --pad-first-paren-out \
+    --unpad-paren --add-braces --convert-tabs \
+    --delete-empty-lines --max-code-length=80)
 
-find "$INP_FOLDER" -name "*.txt" -type f -delete
+JPLAG_JAR="./jplag.jar"
+JPLAG_CMD="java -jar $JPLAG_JAR"
+# -bc "$LIB_NAME" \
+JPLAG_OPTS=(-l "$LANG" -p "$EXTENSIONS" \
+    -v quiet -c normal -r "$OUT_FOLDER" -n "$RES_COUNT")
+
+mkdir -p "$INP_FOLDER/$LIB_NAME"
+unzip -q -o "$LIB_FOLDER" -d "$INP_FOLDER/$LIB_NAME"  > /dev/null 2>&1 || true
+
+
+find "$INP_FOLDER" -name "result.txt" -type f -delete
 find "$INP_FOLDER" -name "*.zip" -type f | while read -r file; do
-    #echo "Unzipping $file"
     zip_path="${file%/*}"
     clean_zip "$file"
     unzip -q -o "$file" -d "$zip_path"
     rm "$file"
 done
 
-#find "$INP_FOLDER" -name "*" -type d -delete
 
-ASTYLE_OPTS=(--quiet --suffix=none --recursive --style=java \
-    --break-blocks --pad-comma --pad-first-paren-out \
-    --unpad-paren --add-braces --convert-tabs \
-    --delete-empty-lines --max-code-length=80)
+clean_in_folder
+
 astyle "$INP_FOLDER/*" "${ASTYLE_OPTS[@]}"
 
+flatten
 
-JPLAG_JAR="./jplag.jar"
-JPLAG_CMD="java -jar $JPLAG_JAR"
-
-OPTS_LANG=(-l "$LANG" -p "$EXTENSIONS" -bc "$LIB_NAME")
-OPTS_GENERAL=(-v quiet -c normal -r "$OUT_FOLDER" -n "$RES_COUNT")
 
 mkdir -p "$OUT_FOLDER" 
-$JPLAG_CMD "${OPTS_GENERAL[@]}" "${OPTS_LANG[@]}" "${INP_FOLDER}" >  "$OUT_FOLDER/jplag.log"
+clean_out_folder
+$JPLAG_CMD "${JPLAG_OPTS[@]}" "${INP_FOLDER}" > "$OUT_FOLDER/jplag.log" || {
+    cat "$OUT_FOLDER/jplag.log"
+    exit 1
+}
+
+head -5 "$OUT_FOLDER/jplag.log"
+tail -5 "$OUT_FOLDER/jplag.log"
 
 python3 inplace.py "$OUT_FOLDER"
-
